94352bd710dfefd32e4c65ff40f2fc3ad7923e5b
stevebanik <stevebanik@gmail.com>
2022-04-07 08:44:29 -0500

    commit messages are mostly not useful

--- commands.sh
+++ commands.sh
@@ -1,4 +1,5 @@
 # list all instances in table format
 aws ec2 describe-instances --query 'Reservations[].Instances[].[Placement.AvailabilityZone, State.Name, InstanceId,InstanceType,Platform,Tags.Value,State.Code,Tags.Values]' --output table
 
-#
+# what's my user id, account and ARN?
+{ aws sts get-caller-identity & aws iam list-account-aliases; } | jq -s ".|add"
